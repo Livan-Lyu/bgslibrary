7734a4bc0742180e74b01f86c3c9d05f93d7cb73
Livan <124090447@link.cuhk.edu.cn>
2026-04-24 20:55:47 +0800

    correct fpga simulate

---
+++ build/CMakeFiles/bgslibrary_core.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/algorithms/ViBe.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/algorithms/ViBe/vibe-background-sequential.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-background-sequential.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-background-sequential.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/algorithms/ViBe/vibe-fpga-sim.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-fpga-sim.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-fpga-sim.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/algorithms/algorithms.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/algorithms.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/algorithms.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/tools/ForegroundMaskAnalysis.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/ForegroundMaskAnalysis.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/ForegroundMaskAnalysis.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/tools/FuzzyUtils.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/FuzzyUtils.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/FuzzyUtils.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/tools/PerformanceUtils.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PerformanceUtils.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PerformanceUtils.cpp.o.d"
+  "/home/livan/research/bgs/bgslibrary/bgslibrary/tools/PixelUtils.cpp" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PixelUtils.cpp.o" "gcc" "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PixelUtils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/bgslibrary_core.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-background-sequential.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-background-sequential.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-fpga-sim.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/ViBe/vibe-fpga-sim.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/algorithms.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/algorithms/algorithms.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/ForegroundMaskAnalysis.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/ForegroundMaskAnalysis.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/FuzzyUtils.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/FuzzyUtils.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PerformanceUtils.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PerformanceUtils.cpp.o.d"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PixelUtils.cpp.o"
+  "CMakeFiles/bgslibrary_core.dir/bgslibrary/tools/PixelUtils.cpp.o.d"
+  "libbgslibrary_core.pdb"
+  "libbgslibrary_core.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bgslibrary_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
